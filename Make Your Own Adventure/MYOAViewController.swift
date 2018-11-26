f9bb3fbaa1bc2538d8b649aae2ec721a6deda537
Felipe Ribeiro <feliperibeiro@me.com>
2018-11-26 11:55:09 -0200

    Melhorias na UX e implementação

--- Make Your Own Adventure/MYOAViewController.swift
+++ Make Your Own Adventure/MYOAViewController.swift
@@ -13,14 +13,14 @@ class MYOAViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.title = "Make Your Own Adventure"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
+        let userIsAtRootViewController = navigationController?.viewControllers.count == 1
+        if !userIsAtRootViewController {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
+        }
     }
     
     @objc func startOver() {
-        if let navigationController = navigationController {
-            navigationController.popToRootViewController(animated: true)
-        }
+        navigationController?.popToRootViewController(animated: true)
     }
     
     deinit {
--- README.md
+++ README.md
@@ -8,3 +8,11 @@ No desenvolvimento deste app foram aprendidos os seguintes conceitos:
 
 * Uso do `UINavigationController`
 * Propriedades `navigationItem` e `navigationController` dos ViewControllers
+
+### Changelog
+
+#### v1.1
+
+* Retira título customizado
+* Retira o botão *Start Over* do início da história
+* Simplificação da action `startOver`
