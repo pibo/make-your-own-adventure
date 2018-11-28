b14af75cff7f7383c545fe8b2bf123210e2a8b6c
Felipe Ribeiro <feliperibeiro@me.com>
2018-11-28 11:04:37 -0200

    ViewController para escolha da aventura adicionado

---
+++ Make Your Own Adventure/NewAdventureViewController.swift
@@ -0,0 +1,53 @@
+//
+//  NewAdventureViewController.swift
+//  Make Your Own Adventure
+//
+//  Created by Felipe Ribeiro on 03/11/18.
+//  Copyright © 2018 Felipe Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class NewAdventureViewController: UITableViewController {
+    
+    // MARK: Properties
+    
+    var adventures = [Adventure]()
+    
+    // MARK: Life Cycle Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let plists = Bundle.main.paths(forResourcesOfType: "plist", inDirectory: nil)
+        
+        for plist in plists {
+            guard (plist as NSString).lastPathComponent != "Info.plist" else { continue }
+            if let adventure = NSDictionary(contentsOfFile: plist) as? AdventureDictionary {
+                adventures.append(Adventure(dictionary: adventure))
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.reloadData()
+    }
+    
+    // MARK: UITableView Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return adventures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AdventureCell")!
+        let adventure = adventures[indexPath.row]
+        
+        cell.textLabel?.text = adventure.credits.title
+        cell.detailTextLabel?.text = adventure.credits.author
+        cell.imageView?.image = adventure.credits.image
+        
+        return cell
+    }
+}
